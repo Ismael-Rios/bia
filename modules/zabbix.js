641fb0cc8eb1080ed1a5dd2f3ac96c7ed756576b
Ismael-Rios <ismaelrios@protonmail.com>
2024-11-08 13:27:35 -0300

    Ajustes no módulo Zabbix, POST já contém mensagem formatada

--- modules/zabbix.js
+++ modules/zabbix.js
@@ -3,28 +3,27 @@ const config = require('../config');
 const { sendMessageToDiscord } = require('./discord');
 const fs = require('fs'); // Módulo para manipular arquivos
 
-function handleZabbixWebhook(data, client) {
-    // Salvando o conteúdo do POST em um arquivo JSON
-    fs.writeFileSync('zabbix_post_data.json', JSON.stringify(data, null, 2), 'utf-8');
-    
+function handleZabbixWebhook(data, client) {    
     /*
-    // Formatação e envio da mensagem ao Discord
-    const alert = data.alert; // message, subject, sendto
-    const event = data.event; // date, id, name, nseverity, opdata, recovery.date, recovery.time, severity, source, tags, time, update.action, update.date, update.message, update.status, update.time, value
-    const host = data.host; // ip, name
-    const trigger = data.trigger; // description, id
+    // ALERT: message, subject, sendto
+    const alert = data.alert || {}; 
+    // EVENT: date, id, name, nseverity, opdata, recovery.date, recovery.time, severity, source, tags, time, update.action, update.date, update.message, update.status, update.time, value
+    const event = data.event || {}; 
+    // HOST: ip, name
+    const host = data.host || {}; 
+    // TRIGGER: description, id
+    const trigger = data.trigger || {}; 
 
     const message = 
-        `${alert.subject}\n` +
-        `${alert.message}\n` +
-        `Host: ${host.name} [${host.ip}]\n` +
-        `Event time: ${event.date} / ${event.time}\n` +
-        `Severity: ${event.severity}\n` +
-        `Opdata: ${event.opdata}`;
+        `${alert.subject || ''}\n` +
+        `${alert.message || ''}\n` +
+        `Host: ${host.name || ''} [${host.ip}]\n` +
+        `Event time: ${event.date || ''} / ${event.time}\n` +
+        `Severity: ${event.severity || ''}\n` +
+        `Opdata: ${event.opdata || ''}`;
     */
 
-    //const messageTeste = `${data}`;
-
+    // POST contendo mensagem já formatada
     sendMessageToDiscord(client, data, config.CHANNEL_ID_ZABBIX);
 }
 
