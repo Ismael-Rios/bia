2eceba1b819c55beb813d4e23c84a6867213d108
Ismael-Rios <ismaelrios@protonmail.com>
2024-11-09 12:41:56 -0300

    Ajuste para novo layout

--- modules/zapsign.js
+++ modules/zapsign.js
@@ -12,19 +12,34 @@ function handleZapsignWebhook(data, client) {
         // Filtra apenas os signatários que têm status 'signed'
         const signers = data.signers
             .filter(signer => signer.email !== "contato@ilhaconnect.net.br") // Exclui MARCOS ASSUNÇÃO
-            .map(signer => `${signer.name}`); // Formata o restante dos signatários
+            .map(signer => `${signer.name}`) // Formata o restante dos signatários
+            .join(', '); // Juntando os nomes em uma string
         
         const signerCPF = data.signers
             .filter(signer => signer.email !== "contato@ilhaconnect.net.br") // Exclui MARCOS ASSUNÇÃO
-            .map(signer => `${signer.cpf}`); // Formata o restante dos signatários
+            .map(signer => `${signer.cpf}`) // Formata o restante dos signatários
+            .join(', '); // Juntando os CPFs em uma string
 
-        const message = `Documento assinado!\n` +
-            `ID do Documento: ${documentId}\n` +
-            `Cliente: ${signers}\n` +
-            `CPF: ${signerCPF}\n` +
-            `Arquivo Assinado: ${signedDocumentInfo}`;
+        const message = {
+            color: 39168,
+            url: signedDocumentInfo,
+            title: "Documento assinado: ${documentId}",
+            footer: {
+                text: `Status: ${status}`
+            },
+            fields: [
+                {
+                    name: "Cliente",
+                    value: signers
+                },
+                {
+                    name: "CPF",
+                    value: signerCPF
+                }
+            ]
+        };
 
-        sendMessageToDiscord(client, message, config.CHANNEL_ID_ZAPSIGN);
+        sendMessageToDiscord(client, { embeds: [message] }, config.CHANNEL_ID_ZAPSIGN);
     }
 }
 
